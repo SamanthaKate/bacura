2c1d6adb4b04a040d984fea9f46859f886ee273a
Samantha Doctora <samanthadoctora@Samanthas-MacBook-Pro.local>
2018-11-06 14:38:35 +1100

    displaying bacura video

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Header from './components/header';
+import BacuraVideo from './videos/bacura.mp4';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <p>
-          Hi my name is Samantha
-        </p>
+
+        <h1>wear<br/>baçura</h1>
+
+        <video
+           className="bacura-video"
+           muted
+           src={BacuraVideo}
+           autoPlay
+           loop
+          >
+          </video>
+
+          <h2>projects@wearbacura.com • <a href="https://www.instagram.com/wearbacura/" target="_blank">insta</a></h2>
+
       </div>
     );
   }
